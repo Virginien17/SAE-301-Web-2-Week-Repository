3853066dc19f176336b1ed651d5477d0d92b3b73
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-11 16:34:08 +0100

    config.php
    Préparation du fichier config
    
    Co-Authored-By: PLANE Antoine - Chef de Projet <191174816+antoineplane-chefproj@users.noreply.github.com>

---
+++ Config/config.php
@@ -0,0 +1,9 @@
+<?php
+    $hote='';
+    $port='';
+    $nom_bd='';
+    $identifiant='';
+    $mot_de_passe='';
+    $encodage='utf8';
+    $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.$encodage);
+?>
