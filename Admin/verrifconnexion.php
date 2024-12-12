6aca532b5267c1e3655164c79de430bcedf357ec
Zoé Coutant <zoe.coutant.basket.56@gmail.com>
2024-12-12 09:28:27 +0100

    Create verrifconnexion.php
    création et début de rédaction du code pour vérifier si la connexion à un compte est bonne

---
+++ Admin/verrifconnexion.php
@@ -0,0 +1,31 @@
+<?php
+    include("../Config/config.php");
+    $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nom_bd,$identifiant, $mot_de_passe,$options);
+
+    // on doit lister l'ensemble des produit du site, ordonnés par date décroissante pour la suppression
+    $requete='SELECT mail, mdp FROM `admin`';
+    $resultats=$bdd->query($requete);
+    $admin=$resultats->fetch(PDO::FETCH_ASSOC);
+    $resultats->closeCursor();
+
+    // on doit lister les catégories existante
+    $requete='SELECT mail, mdp FROM `compte`';
+    $resultats=$bdd->query($requete);
+    $compte=$resultats->fetchAll(PDO::FETCH_ASSOC);
+    $resultats->closeCursor()
+
+
+    if ($_POST["mail"]==$admin["mail"] && $_POST["mdp"]===$admin["mdp"])
+    {
+        header('Location: admin.php');
+        exit();
+    }
+
+    
+
+    else
+    {
+        header('Location: ../index.php');
+        exit();
+    }
+?>
