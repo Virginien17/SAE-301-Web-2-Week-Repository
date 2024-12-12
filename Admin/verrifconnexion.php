336b48a71007d9a06526d2198a1a6a3c09b11743
Zoé Coutant <zoe.coutant.basket.56@gmail.com>
2024-12-12 09:40:05 +0100

    ajout d'information
    Ajout des infos de connexion à la bdd

--- Admin/verrifconnexion.php
+++ Admin/verrifconnexion.php
@@ -2,13 +2,11 @@
     include("../Config/config.php");
     $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nom_bd,$identifiant, $mot_de_passe,$options);
 
-    // on doit lister l'ensemble des produit du site, ordonnés par date décroissante pour la suppression
     $requete='SELECT mail, mdp FROM `admin`';
     $resultats=$bdd->query($requete);
     $admin=$resultats->fetch(PDO::FETCH_ASSOC);
     $resultats->closeCursor();
 
-    // on doit lister les catégories existante
     $requete='SELECT mail, mdp FROM `compte`';
     $resultats=$bdd->query($requete);
     $compte=$resultats->fetchAll(PDO::FETCH_ASSOC);
@@ -20,10 +18,12 @@
         exit();
     }
 
-    
+    if ($_POST["mail"]==$admin["mail"] && $_POST["mdp"]==$admin["mdp"]) {
+        header('Location: ../compte.php');
+        exit();
+    }
 
-    else
-    {
+    else {
         header('Location: ../index.php');
         exit();
     }
--- Config/config.php
+++ Config/config.php
@@ -1,8 +1,8 @@
 <?php
-    $hote='';
-    $port='';
-    $nom_bd='';
-    $identifiant='';
+    $hote='localhost';
+    $port='3306';
+    $nom_bd='mumo';
+    $identifiant='root';
     $mot_de_passe='';
     $encodage='utf8';
     $options=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.$encodage);
